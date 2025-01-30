213c919c1cdd94785c8389f43712f65c520ce97b
Sahba-M <sahba.mir.2005@gmail.com>
2025-01-30 21:39:31 +0330

    Edit the Implementation of right shift register

--- rshift.vhd
+++ rshift.vhd
@@ -1,59 +1,68 @@
 LIBRARY IEEE ;
 USE IEEE.STD_LOGIC_1164.ALL ;
-USE IEEE.STD_LOGIC_ARITH.ALL ;
-USE IEEE.STD_LOGIC_UNSIGNED.ALL ;
+USE IEEE.NUMERIC_STD.ALL ;
 
-ENTITY Right_Shift IS 
+ENTITY Right_Shift_Register IS 
     GENERIC ( size : INTEGER := 4 );
     PORT (
         CLK : IN STD_LOGIC ;
         Reset : IN STD_LOGIC ;
+        Shift : IN STD_LOGIC ; 
         A : IN STD_LOGIC_VECTOR ( size - 1 DOWNTO 0 );
-        Amount : IN INTEGER ; --OPTIONAL
         Result : OUT STD_LOGIC_VECTOR ( size - 1 DOWNTO 0 );
         Zero_flag : OUT STD_LOGIC ;
         Carry_flag : OUT STD_LOGIC ;
         Borrow_flag : OUT STD_LOGIC ;
         Overflow_flag : OUT STD_LOGIC ;
         Greater_flag : OUT STD_LOGIC ;
-        Equal_flag : OUT STD_LOGIC ;
+        Equal_flag : OUT STD_LOGIC 
     );
-END Right_Shift ;
+END Right_Shift_Register ;
 
-ARCHITECTURE Behavior OF Right_Shift IS 
+ARCHITECTURE Behavior OF Right_Shift_Register IS 
+    SIGNAL Reg : STD_LOGIC_VECTOR ( size - 1 DOWNTO 0 );
 BEGIN 
-    VARIABLE temp : STD_LOGIC_VECTOR ( size DOWNTO 0 );
-    PROCESS( CLK , Reset );
-        if ( Reset = 1 ) then 
-            Result <= ( OTHERS => '0' );
-            Zero_flag <= '0';
-            Carry_flag <= '0';
-            Borrow_flag <= '0';
-            Overflow_flag <= '0';
-            Greater_flag <= '0';
-            Equal_flag <= '0';
+    PROCESS( CLK , Reset )
+    BEGIN
+        if ( Reset = '1' ) then 
+            Reg <= ( OTHERS => '0' );
         elsif rising_edge (CLK) then
-            temp <= A ;
-            for i in 0 to Amount - 1 LOOP 
-                Carry_flag <= temp(0);
-                temp = "0" & temp ( size - 1 DOWNTO 1 );
-            END LOOP ;
-            Result <= temp ;
+            if Shift = '1' then 
+                Reg <= "0" & Reg ( size - 1 DOWNTO 1 );
+            else
+                Reg <= A ;
+            end if ;
         end if ;
+    END PROCESS ;
+        
+    Result <= Reg ;
 
-        if Result = (others => '0') then
-            Zero_flag <= '1';
-        else
-            Zero_flag <= '0';
-        end if ;
+    PROCESS ( Reg , A )
+    BEGIN 
+
+        Zero_flag <= '1';
+        for i in 0 to size-1 loop
+            if reg(i) = '1' then
+                Zero_flag <= '0';
+            end if ;
+        end loop ;
        
-        Carry_flag <= Carry_flag ;
+        Carry_flag <= Reg(0) ;
         Borrow_flag <= '0'; 
         Overflow_flag <= '0'; 
-        Greater_flag <= '0'; 
-        Equal_flag <= '0'; 
-        
+        if unsigned(Reg) > unsigned(A) then
+            Greater_flag <= '1';
+        else
+            Greater_flag <= '0';
+        end if;
+
+        if unsigned(Reg) = unsigned(A) then
+            Equal_flag <= '1';
+        else
+            Equal_flag <= '0';
+        end if;
     END PROCESS ;
+
 END Behavior ;
 
 
